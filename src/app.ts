45497c1963185a4b889ca8c46f92b6f3329f5e23
Guilherme G <98757828+GuilhermeGavioli@users.noreply.github.com>
2022-06-27 22:05:15 -0300

    ....

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-const express = require('express');
-const http = require("http")
-const cors = require("cors")
-const path = require("path")
-const dotenv = require("dotenv")
+import express from 'express'
+import http from "http"
+import cors from "cors"
+import path from "path"
+import dotenv from "dotenv"
 dotenv.config();
 
-const { Server } = require("socket.io")
+import { Server } from "socket.io"
 
 
 const app = express();
@@ -15,23 +15,20 @@ app.use(express.static(path.join(__dirname + "/..", "/public")));
 app.use(cors())
 
 
-let users = [
-    "test-user",
-]
 
 
 let messages = {
     room1: [
-        {owner: "Chat Bot", message:"Welcome to Room 1", time: ""},
+        {owner: "Chat Bot", ownerID: "0", message:"Welcome to Room 1", time: ""},
     ],
     room2: [
-        {owner: "Chat Bot", message:"Welcome to Room 2", time: ""},
+        {owner: "Chat Bot", ownerID: "0", message:"Welcome to Room 2", time: ""},
     ],
     room3: [
-        {owner: "Chat Bot", message:"Welcome to Room 3", time: ""},
+        {owner: "Chat Bot", ownerID: "0", message:"Welcome to Room 3", time: ""},
     ],
     room4: [
-        {owner: "Chat Bot", message:"Welcome to Room 4", time: ""},
+        {owner: "Chat Bot", ownerID: "0", message:"Welcome to Room 4", time: ""},
     ],
 }
 
@@ -55,6 +52,15 @@ io.on("connection", (socket) => {
         switch (data.room) { 
             case "room1":
                 messages.room1.push({owner: data.owner, ownerID: data.ownerID, message: data.message, time: date.getHours() + ":" +  date.getMinutes()})
+                break;
+            case "room2":
+                messages.room2.push({owner: data.owner, ownerID: data.ownerID, message: data.message, time: date.getHours() + ":" +  date.getMinutes()})
+                break;
+            case "room3":
+                messages.room3.push({owner: data.owner, ownerID: data.ownerID, message: data.message, time: date.getHours() + ":" +  date.getMinutes()})
+                break;
+            case "room4":
+                messages.room4.push({owner: data.owner, ownerID: data.ownerID, message: data.message, time: date.getHours() + ":" +  date.getMinutes()})
                 break;
         }
         socket.to(data.room).emit("send-message-callback", { owner: data.owner, message: data.message, status: 200, ownerID: data.ownerID, time: date.getHours() + ":" +  date.getMinutes()})
@@ -91,8 +97,7 @@ app.get("/chat", (req, res) => {
     const { room, nickname } = req.query
     if (room !== "room1" && room !== "room2" && room !== "room3" && room !== "room4") { return res.json({ error: "Room does not exist" }) }
    
-    
-    users.push(nickname)
+
 
     if (!room || !nickname) return res.json({error: "No nickname or room provided..."})
     res.sendFile(path.join(__dirname + "/.." + "/public/chat.html"))
